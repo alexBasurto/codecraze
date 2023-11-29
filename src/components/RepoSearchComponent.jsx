f2b73846fe93f62a30ef39fbced04a7790dacfcc
Alex Basurto <alexbasurto1@gmail.com>
2023-11-29 16:46:50 +0100

    Fn consulta API aislada y reutilizable

--- src/components/RepoSearchComponent.jsx
+++ src/components/RepoSearchComponent.jsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
 import { useEffect } from "react";
+
 import RepoForm from "./RepoFormComponent";
 import RepoRow from "./RepoRowComponent";
 import RepoFile from "./RepoFileComponent";
+
+import getReposFromAPI from "./utils/apiGitHub.js";
 
 const RepoSearch = () => {
   const [loaded, setLoaded] = useState(false);
@@ -12,6 +15,8 @@ const RepoSearch = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [perPage, setPerPage] = useState(30);
   const [totalPages, setTotalPages] = useState(null);
+  const [sortBy, setSortBy] = useState("stars");
+  const [orderBy, setOrderBy] = useState("desc");
   const [currentSearch, setCurrentSearch] = useState([]);
   const [searchOrFile, setSearchOrFile] = useState(true); // true = search, false = file
 
@@ -20,36 +25,29 @@ const RepoSearch = () => {
   useEffect(() => {
   }, [currentPage]);
 
-  const getRepos = async (query) => {
-    try {
-      const data = await fetch(query);
-      const results = await data.json();
-      setTotalPages(Math.ceil(results.total_count / perPage)); // Calcular el número total de páginas
-      setRepoList(results.items);
-    } catch (e) {
-      setError("Algo salió mal...");
-      console.error(e);
-    }
-  }  
-
-  const buildQuery = (formData, page) => {
-    let query = 'https://api.github.com/search/repositories';
-    query += `?q=stars:%3E2000`;
-    if (formData.repoName) query +=  `+${formData.repoName}+in:name`; //ok
-    if (formData.repoDesc) query +=  `+${formData.repoDesc}+in:description`; //ok
-    if (formData.owner) query +=  `+user:${formData.owner}`; //ok
-    if (formData.language) query +=  `+language:${formData.language}`; //ok
-    if (formData.license) query +=  `+license:${formData.license}`; //ok
-    query += `&sort=stars&order=desc&page=${page}&per_page=${perPage}`; //ok
-    console.log("After conditions:", query);
-    return query;
-  };
-
   const handleFormSubmit = (formData) => {
     setCurrentSearch(formData);
-    const query = buildQuery(formData, currentPage);
+    const queryParams = {
+      name: formData.repoName,
+      description: formData.repoDesc,
+      owner: formData.owner,
+      language: formData.language,
+      license: formData.license,
+      limit: "stars:>2000",
+      sort: sortBy,
+      order: orderBy,
+      page: currentPage,
+      per_page: perPage,
+    }
     setRepoList([]);
-    getRepos(query);
+    getReposFromAPI(formData, queryParams).then((data) => {
+      if (data.items) {
+        setRepoList(data.items);
+        setTotalPages(Math.ceil(data.total_count / perPage));
+      } else {
+        setError(data.message);
+      }
+    });
   };
 
   const handleOpenRepoFile = (repo) => {
---
+++ src/components/utils/apiGitHub.js
@@ -0,0 +1,32 @@
+// Aislando la lógica de la API de GitHub en un archivo aparte para poder reutilizarla en otros componentes. Principio DRY (Don't Repeat Yourself)
+
+const getReposFromAPI = async (formData, queryParams) => {
+    let query = "";
+    if (!queryParams) {
+        query =
+            "https://api.github.com/search/repositories?q=stars:%3E2000&sort=stars&order=desc&page=1&per_page=100";
+    } else {
+        query = "https://api.github.com/search/repositories";
+        query += `?q=${queryParams.limit}`;
+        if (queryParams.name) query += `+${queryParams.name}+in:name`; //ok
+        if (queryParams.description)
+            query += `+${queryParams.description}+in:description`; //ok
+        if (queryParams.owner) query += `+user:${queryParams.owner}`; //ok
+        if (queryParams.language) query += `+language:${queryParams.language}`; //ok
+        if (queryParams.license) query += `+license:${queryParams.license}`; //ok
+        query += `&sort=${queryParams.sort}`;
+        query += `&order=${queryParams.order}`;
+        query += `&page=${queryParams.page}`;
+        query += `&per_page=${queryParams.per_page}`;
+    }
+    try {
+        const response = await fetch(query);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        return error;
+    }
+};
+
+
+export default getReposFromAPI;
