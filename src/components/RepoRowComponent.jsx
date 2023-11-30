83633a4e59e0c928fe98f3f44c896cef50225da2
Alex Basurto <alexbasurto1@gmail.com>
2023-11-30 12:07:17 +0100

    Estilos tablas

--- src/components/RepoRowComponent.jsx
+++ src/components/RepoRowComponent.jsx
@@ -12,11 +12,11 @@ const RepoRow = ({ data , openRepoFile}) => {
     <tbody>
     <tr className={className + (loaded ? "" : " hidden")} onClick={handleClick}>
         <td>{data.name.toUpperCase()}</td>
-        <td>{data.description}</td>
+        <td className="description">{(data.description.length > 60) ? (data.description.substr(0, 60) + "...") : data.description}</td>
         <td>{data.language}</td>
         <td>{data.stargazers_count}</td>
-        <td>
-        <img src={data.owner.avatar_url} alt="Owner profile" className="ownerMiniature" />{data.owner.login}
+        <td className="owner">
+        <img src={data.owner.avatar_url} alt="Owner profile" className="ownerMiniature" /><p>{data.owner.login}</p>
         </td>
         <td>{data.owner.type}</td>
 
